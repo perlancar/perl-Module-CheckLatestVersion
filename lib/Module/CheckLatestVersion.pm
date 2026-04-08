13037f8b3cd05c32bf8462354d07d7938beff0cf
perlancar <perlancar@gmail.com>
2026-04-08 10:39:36 +0700

    Observe PERL_MODULE_CHECKLATESTVERSION_SKIP

--- lib/Module/CheckLatestVersion.pm
+++ lib/Module/CheckLatestVersion.pm
@@ -17,7 +17,8 @@ sub check_latest_version {
     return if
         $ENV{HARNESS_ACTIVE} ||
         $ENV{RELEASE_TESTING} ||
-        $ENV{AUTOMATED_TESTING};
+        $ENV{AUTOMATED_TESTING} ||;
+        $ENV{PERL_MODULE_CHECKLATESTVERSION_SKIP};
 
     no strict 'refs'; ## no critic: TestingAndDebugging::ProhibitNoStrict
 
@@ -79,6 +80,15 @@ message is displayed. If the C<die> option is set, program will die.
 
 =head1 DESCRIPTION
 
+This module can be used to check other module's version against latest version
+in authority. Authority can be CPAN or DarkPAN or other schemes that are
+supported by L<Module::CheckVersion>.
+
+Checking against authority is cached, by default 3600 seconds (default from
+L<Cache::File::Simple>).
+
+This can be used to ensure that scripts use the latest version of a module.
+
 
 =head1 FUNCTIONS
 
@@ -93,7 +103,8 @@ L<Module::CheckVersion>. C<$mod> defaults to the caller's package. If module is
 not the latest version, a warning is emitted.
 
 When one of these environment variables are set, will skip checking (no-op):
-C<HARNESS_ACTIVE>, C<RELEASE_TESTING>, C<AUTOMATED_TESTING>.
+C<HARNESS_ACTIVE>, C<RELEASE_TESTING>, C<AUTOMATED_TESTING>,
+C<PERL_MODULE_CHECKLATESTVERSION_SKIP>.
 
 Options:
 
@@ -111,6 +122,10 @@ Bool. If set to true, will die instead of warn.
 =head2 PERL_MODULE_CHECKLATESTVERSION_OPT_DIE
 
 Bool. Set default value for the C<die> option.
+
+=head2 PERL_MODULE_CHECKLATESTVERSION_SKIP
+
+Bool. Can be set to true to skip checking.
 
 
 =head1 SEE ALSO
